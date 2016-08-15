7121949164198e9311d2803e29fdb495c23e297e
Golars <titorens@gmail.com>
2016-08-15 12:47:47 +0300

    +fix css files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 console.log('MAIN');
 console.log($('.container'));
-$('.m-slide-right').on('click', function(){
+
+$('.m-slide-right').on('click', function () {
     var $selected = $('.m-slide-el:not(.active)');
     var $active = $('.m-slide-el.active');
 
     $active.removeClass('active');
     $selected.addClass('active');
 })
+
+document.getElementsByTagName('div');
+
+$('div')
+document.getElementById('id')
+$('#id')
