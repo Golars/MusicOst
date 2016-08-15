0c97bf55da631ebd79c172839dce796b4dd42260
Golars <titorens@gmail.com>
2016-08-15 12:48:28 +0300

    +FIX JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-console.log('MAIN');
-console.log($('.container'));
-
 $('.m-slide-right').on('click', function () {
     var $selected = $('.m-slide-el:not(.active)');
     var $active = $('.m-slide-el.active');
@@ -8,9 +5,3 @@ $('.m-slide-right').on('click', function () {
     $active.removeClass('active');
     $selected.addClass('active');
 })
-
-document.getElementsByTagName('div');
-
-$('div')
-document.getElementById('id')
-$('#id')
